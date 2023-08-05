5c72516b92e30808535706902db034c092546b9e
Pasindu Sampath <104705509+pasindusampath@users.noreply.github.com>
2023-08-05 14:34:52 +0530

    Implemented Dynamically Changing Response Using Response Entity

--- src/main/java/lk/ijse/gdse63/test/api/Item.java
+++ src/main/java/lk/ijse/gdse63/test/api/Item.java
@@ -2,10 +2,13 @@ package lk.ijse.gdse63.test.api;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
 
 @RestController
 @RequestMapping("item")
@@ -23,5 +26,15 @@ public class Item {
     public String saveOrder1(){
         System.out.println("XML");
         return "X-ML Order";
+    }
+
+
+    @PostMapping(value = "saveOrUpdate" , consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> saveOrUpdate(){
+        if(new Random().nextInt(2)<1){
+            return new ResponseEntity<>("JSON", HttpStatus.CREATED);
+        }else {
+            return new ResponseEntity<>("JSON", HttpStatus.NO_CONTENT);
+        }
     }
 }
